82e8d97d08acfa9455649c4a4010904906b732c2
Quentin Jungo <quentin.jungo@gmail.com>
2022-08-14 18:52:51 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,82 @@
 use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    event::{poll, read, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use std::{io, thread, time::Duration};
+use std::io;
 use tui::{
-    backend::CrosstermBackend,
+    backend::{Backend, CrosstermBackend},
     layout::{Constraint, Direction, Layout},
-    widgets::{Block, Borders, Widget},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans},
+    widgets::{Block, Borders, /*Widget,*/ Paragraph},
     Terminal,
 };
 
+fn render<B: Backend>(terminal: &mut Terminal<B>, sentence: &String) {
+    terminal
+        .draw(|f| {
+            let layout = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(1)
+                .constraints(
+                    [
+                        Constraint::Percentage(5),
+                        Constraint::Percentage(80),
+                        Constraint::Percentage(10),
+                    ]
+                    .as_ref(),
+                )
+                .split(f.size());
+
+            let title_block = Block::default()
+                .title("KeyboardRacer")
+                .borders(Borders::NONE)
+                .style(
+                    Style::default()
+                        .fg(Color::Blue)
+                        .add_modifier(Modifier::BOLD | Modifier::ITALIC),
+                );
+
+            let job_block = Block::default().borders(Borders::ALL);
+
+            let p = Paragraph::new(vec![Spans::from(vec![Span::raw(sentence)])]).block(job_block);
+
+            f.render_widget(title_block, layout[0]);
+            f.render_widget(p, layout[1]);
+        })
+        .unwrap();
+}
+
 fn main() -> Result<(), io::Error> {
     // setup terminal
-
     enable_raw_mode()?;
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
+    let mut sentence = String::new();
 
-    terminal.draw(|f| {
-        let size = f.size();
-        let block = Block::default().title("Block").borders(Borders::ALL);
-        f.render_widget(block, size);
-    })?;
+    // programme loop
+    render(&mut terminal, &sentence);
+    loop {
+        match read()? {
+            Event::Key(e) => {
+                match e.code {
+                    KeyCode::Char(c) => sentence.push(c),
+                    KeyCode::Esc => break,
+                    KeyCode::Backspace => {
+                        sentence.pop();
+                    }
+                    _ => (),
+                }
+                render(&mut terminal, &sentence);
+            }
+            _ => (),
+        }
+    }
 
-    thread::sleep(Duration::from_millis(5000));
-
-    // restore terminal
+    // restore terminal when the loop is breaked
     disable_raw_mode()?;
     execute!(
         terminal.backend_mut(),
@@ -36,6 +84,5 @@ fn main() -> Result<(), io::Error> {
         DisableMouseCapture
     )?;
     terminal.show_cursor()?;
-
     Ok(())
 }
