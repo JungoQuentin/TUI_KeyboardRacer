375840c07bfb3e9fa8f09c0a79a42d94519b9722
Quentin <quentin.jungo@gmail.com>
2022-11-15 23:41:28 +0100

    delta add

--- src/main.rs
+++ src/main.rs
@@ -4,70 +4,86 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use std::io;
-use tui::{
-    backend::{Backend, CrosstermBackend},
-    //layout::{Constraint, Direction, Layout},
-    //style::{Color, Modifier, Style},
-    //text::{Span, Spans},
-    //widgets::{Block, Borders, /*Widget,*/ Paragraph},
-    Terminal,
-};
+use std::time::Instant;
+use tui::{self, backend::CrosstermBackend, Terminal};
 mod render;
-use render::{render};
+use render::render;
 
-fn main() -> Result<(), io::Error> {
-    // ??
+fn init_term() -> Result<Terminal<tui::backend::CrosstermBackend<io::Stdout>>, io::Error> {
     enable_raw_mode()?;
-    // variable 
+    // variable
     let mut stdout = io::stdout();
     // Met le fond noir -> rentre dans
     // un autre ecran
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-    // 
+    //
     let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
+    let terminal: Terminal<tui::backend::CrosstermBackend<io::Stdout>> = Terminal::new(backend)?;
+    Ok(terminal)
+}
 
+pub struct CeQueJeMetDansRender {
+    sentence: String,
+}
 
-    let mut sentence = String::new();
-
-    // render the first frame
-    // All the other frame will be rendered after an action
-    render(&mut terminal, &sentence);
-    
-    loop {
-        match read()? {
-            Event::Key(e) => {
-                match e.code {
-                    KeyCode::Char(c) => sentence.push(c),
-                    KeyCode::Esc => break,
-                    KeyCode::Backspace => {
-                        sentence.pop();
-                    }
-                    _ => (),
-                }
-                render(&mut terminal, &sentence);
-            }
-            Event::Mouse(_e) => {
-                //sentence.push('a');
-                //render(&mut terminal, &sentence);
-
-            }
-            _ => (),
-        }
-    }
-
-    // restore terminal when the loop is breaked
-    disable_raw_mode()?;
+fn restore_term(terminal: &mut Terminal<tui::backend::CrosstermBackend<io::Stdout>>) {
+    disable_raw_mode().unwrap();
     execute!(
         terminal.backend_mut(),
         LeaveAlternateScreen,
         DisableMouseCapture
-    )?;
-    terminal.show_cursor()?;
+    )
+    .unwrap();
+    terminal.show_cursor().unwrap();
+}
+
+fn delta_time(start: Instant) -> f64 {
+    let end = Instant::now();
+    let delta = end.duration_since(start).as_secs_f64();
+    delta
+}
+fn main() -> Result<(), io::Error> {
+    let mut terminal = init_term()?;
+    let mut render_infos = CeQueJeMetDansRender {
+        sentence: String::from(""),
+    };
+    // render the first frame
+    // All the other frame will be rendered after an action
+    render(&mut terminal, &render_infos, 0.0);
+    let mut last_render = std::time::Instant::now();
+
+    loop {
+        match read()? {
+            Event::Key(e) => {
+                if event_handler(e.code, &mut render_infos)? == false {
+                    break;
+                } else {
+                    render(
+                        &mut terminal,
+                        &render_infos,
+                        delta_time(last_render),
+                    );
+                    last_render = Instant::now();
+                }
+            }
+            _ => (),
+        }
+    }
+    restore_term(&mut terminal);
     Ok(())
 }
 
-pub fn event_handler(code: KeyCode) -> Result<(), io::Error> {
-    Ok(())
+pub fn event_handler(
+    code: KeyCode,
+    render_infos: &mut CeQueJeMetDansRender,
+) -> Result<bool, io::Error> {
+    match code {
+        KeyCode::Esc => return Ok(false),
+        KeyCode::Char(c) => render_infos.sentence.push(c),
+        KeyCode::Backspace => {
+            render_infos.sentence.pop();
+        }
+        _ => (),
+    }
+    Ok(true)
 }
-
--- src/render.rs
+++ src/render.rs
@@ -1,51 +1,78 @@
+use crate::CeQueJeMetDansRender;
 use tui::{
-    backend::{Backend},
+    backend::Backend,
     layout::{Constraint, Direction, Layout},
     style::{Color, Modifier, Style},
+    //symbols::Marker,
     text::{Span, Spans},
-    widgets::{Block, Borders, /*Widget,*/ Paragraph},
+    widgets::{canvas::*, Block, Borders, Paragraph},
     Terminal,
 };
 
-fn main_block(sentence: &String) -> Paragraph {
-    let main_block = Block::default().borders(Borders::ALL);
-    let p = Paragraph::new(vec![Spans::from(vec![Span::raw(sentence)])]).block(main_block);
-    return p;
-}
+// TODO ailleurs ?
+const BLOCK_CHAR: char = '\u{2588}';
 
 fn title_block() -> Block<'static> {
-   let title_block = Block::default()
-       .title("KeyboardRacer")
-       .borders(Borders::NONE)
-       .style(
-           Style::default()
-               .fg(Color::Blue)
-               .add_modifier(Modifier::BOLD | Modifier::ITALIC),
-       );
-   return title_block;
+    let title_block = Block::default()
+        .title("KeyboardRacer")
+        .borders(Borders::NONE)
+        .style(
+            Style::default()
+                .fg(Color::Blue)
+                .bg(Color::Black)
+                .add_modifier(
+                    Modifier::BOLD | Modifier::ITALIC, /*| Modifier::RAPID_BLINK */
+                ),
+        );
+    title_block
 }
 
-fn set_vertical_layout<B: Backend>(split: Vec<u16>, frame: &mut tui::Frame<B>) -> Vec<tui::layout::Rect> {
-    let mut constraints:Vec<Constraint> = Vec::new();
+fn main_block(sentence: &String, display_block: bool) -> Paragraph {
+    let main_block = Block::default().borders(Borders::ALL);
+    let mut spans = vec![Span::styled(
+        sentence,
+        Style::default()
+            .fg(Color::White)
+            .bg(Color::Black)
+            .add_modifier(Modifier::BOLD),
+    )];
+    if display_block {
+        spans.push(Span::raw(BLOCK_CHAR.to_string()));
+    }
+    Paragraph::new(vec![Spans::from(spans)])
+        .style(Style::default().fg(Color::White).bg(Color::Black))
+        .block(main_block)
+}
+
+fn set_vertical_layout<B: Backend>(
+    split: Vec<u16>,
+    frame: &mut tui::Frame<B>,
+) -> Vec<tui::layout::Rect> {
+    let mut constraints: Vec<Constraint> = Vec::new();
     for s in split {
         constraints.push(Constraint::Percentage(s));
     }
-            let layout = Layout::default()
-                .direction(Direction::Vertical)
-                .margin(1)
-                .constraints(constraints.as_ref())
-                .split(frame.size());
-            return layout;
+    let layout = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints(constraints.as_ref())
+        .split(frame.size());
+    layout
 }
 
-pub fn render<B: Backend>(terminal: &mut Terminal<B>, sentence: &String) {
+pub fn render<B: Backend>(
+    terminal: &mut Terminal<B>,
+    render_infos: &CeQueJeMetDansRender,
+    delta: f64,
+) {
     terminal
         .draw(|frame| {
-            let layout = set_vertical_layout(vec![5, 80], frame);
+            let layout = set_vertical_layout(vec![5, 50, 45], frame);
 
             // Render blocks
+            let main_block = main_block(&render_infos.sentence, delta < 0.5);
             frame.render_widget(title_block(), layout[0]);
-            frame.render_widget(main_block(sentence), layout[1]);
+            frame.render_widget(main_block, layout[1]);
         })
         .expect("It didn't render. Why ?");
 }
